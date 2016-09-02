9a2c3153fba991fcbc574f1516f88313c114006c
Salimuch <Salimyanov.A.R@gmail.com>
2016-09-02 19:00:51 +0300

    initial step

---
+++ Calculator/GraphicView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphicView.swift
+//  Calculator
+//
+//  Created by Артем on 02/09/16.
+//  Copyright © 2016 Artem Salimyanov. All rights reserved.
+//
+
+import UIKit
+
+class GraphicView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Assignment-2-2016-Calculator
-
-## available versions:
