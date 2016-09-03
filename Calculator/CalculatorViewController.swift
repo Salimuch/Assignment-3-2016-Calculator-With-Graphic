ee24b47c6248b8c3937de2a8b4aec09518815538
Salimuch <Salimyanov.A.R@gmail.com>
2016-09-03 23:28:38 +0300

    start from calc

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CalculatorViewController: UIViewController {
+class CalculatorViewController: UIViewController, UISplitViewControllerDelegate {
     
     @IBOutlet private weak var display: UILabel!
     @IBOutlet private weak var history: UILabel!
@@ -91,6 +91,30 @@ class CalculatorViewController: UIViewController {
         }
         displayValue = brain.result
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        splitViewController?.delegate = self
+    }
+    
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController, ontoPrimaryViewController primaryViewController: UIViewController) -> Bool {
+        if primaryViewController.contentViewController == self {
+            if let ivc = secondaryViewController.contentViewController as? GraphicViewController {
+                return true
+            }
+        }
+        return false
+    }
 
+}
+
+extension UIViewController {
+    var contentViewController: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
 }
 
